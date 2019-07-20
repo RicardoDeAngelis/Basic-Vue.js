4ab6e0a6f8cfce61ed46090dff215c5abcfb91c8
Ricardo <deangelisricardoexequiel@gmail.com>
2019-07-20 01:35:58 -0300

    Terminado esta verga

---
+++ ricardo-music/src/API/config.js
@@ -0,0 +1,15 @@
+// ESto sera un modulo de js que exportara
+// un objeto con los datos
+// API account created
+// Here are the details of your new API account.
+// Application name 	RicardoMusic
+// API key 	240bec04f8759009819f2d306ef35ed3
+// Shared secret 	d5a378f1629ab843ec3264e6c8d47a7d
+// Registered to 	RicardoExe
+ export default {
+   appName:'ricardomusic',
+   apikey: '240bec04f8759009819f2d306ef35ed3',
+   secret: 'd5a378f1629ab843ec3264e6c8d47a7d',
+   registeredTo: 'RicardoExe',
+   
+ }
