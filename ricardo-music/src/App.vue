ae86e2971e929eca283af925295e549befaaa59d
Ricardo <deangelisricardoexequiel@gmail.com>
2019-07-20 02:18:00 -0300

    Fixed a la URL  verga

--- ricardo-music/src/App.vue
+++ ricardo-music/src/App.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   #app
-   img(src='./assets/logo.png')
+   img(src='https://ricardodeangelis.github.io/Basic-Vue.js/dist/logo.png')
    h1 RicardoMusic
    select(v-model="selectedCountry")
      option(v-for="country in countries" :value="country.value") {{ country.name }} 
